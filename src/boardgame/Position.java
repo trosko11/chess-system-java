6b668fc91e0213776318143e79ebcaf34063db84
trosko11 <moisesterleschi@hotmail.com>
2022-12-20 16:02:43 -0300

    ChessException and ChessPosition

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -10,21 +10,17 @@ public class Position {
 		this.column = column;
 	}
 
-
 	public int getRow() {
 		return row;
 	}
-
 
 	public void setRow(int row) {
 		this.row = row;
 	}
 
-
 	public int getColumn() {
 		return column;
 	}
-
 
 	public void setColumn(int column) {
 		this.column = column;
