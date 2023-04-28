40ef4cc1b6fc9a59b5bfabfaa5694bb26628ccb8
trosko11 <moisesterleschi@hotmail.com>
2023-04-28 14:44:55 -0300

    CHessPiece piece

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -23,7 +23,7 @@ public abstract class ChessPiece extends Piece {
 	
 	
 	protected boolean isThereOpponentPiece(Position position) {
-		ChessPiece p = (ChessPiece) getBoard().piece(position);
-		return p != null && p.getColor() != color;
+		ChessPiece piece = (ChessPiece) getBoard().piece(position);
+		return piece != null && piece.getColor() != color;
 	}
 }
